71170d2d7c060a60577acc2fbc5ab2650e7c7713
leosimoeslb <leonardo.simoes.consultor@londonbridge.com.br>
2025-08-06 20:19:48 -0300

    feat: first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(map[string]string{"message": "Hello World!"}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	log.Default().Println("Server is running on http://localhost:8082")
+	log.Fatal(http.ListenAndServe(":8082", router))
+}
